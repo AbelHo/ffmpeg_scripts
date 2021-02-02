a7f68479515e44a06de999205a087b398c1b4f21
AbelHo <21101710+AbelHo@users.noreply.github.com>
2021-02-02 17:46:45 +0800

    binary file to wav file conversion

---
+++ bin2wav.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# usage: bin2wav.sh [folder_path] [fs] [number_of_channels] [outputfolder]
+
+if [ $# -eq 0 ]
+then
+  echo 'bin2wav.sh [folder_path] [fs] [number_of_channels] [outputfolder]'
+else
+  cd $1
+fi
+
+if [ $# -eq 4 ]
+then
+  outfol=$4
+else
+  outfol="."
+fi
+
+
+for f in `ls *.mkv`; do ffmpeg -f s16le -ar $2 -ac $3 -i $f -f wav $outfol/${f%%.bin}.wav -loglevel quiet; done
