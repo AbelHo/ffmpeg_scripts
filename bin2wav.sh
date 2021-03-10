1ad14d8a168bde059085477925b461710ce51010
AbelHo <abel@arl.nus.edu.sg>
2021-03-11 02:07:52 +0800

    fix(bin2wav.sh): fix pathname with spaces, change default output folder to $1_wav

--- bin2wav.sh
+++ bin2wav.sh
@@ -13,9 +13,9 @@ if [ $# -eq 4 ]
 then
   outfol=$4
 else
-  outfol="."
+  outfol="$1_wav"
 fi
 
-mkdir $outfol || echo 'cannot create directory'
+mkdir "$outfol" || echo 'cannot create directory'
 
-for f in `ls *.bin`; do ffmpeg -f s16le -ar $2 -ac $3 -i $f -f wav $outfol/${f%%.bin}.wav -loglevel quiet; done
+for f in `ls *.bin`; do ffmpeg -f s16le -ar $2 -ac $3 -i $f -f wav "$outfol/${f%%.bin}.wav" -loglevel quiet -n; done
