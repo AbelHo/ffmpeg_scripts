38f0cd0506e8e50ac7526c4dea97b976dc18a897
AbelHo <21101710+AbelHo@users.noreply.github.com>
2023-01-03 18:40:23 +0800

    feat(v2p.sh): added argument feature and fps assignment

--- v2p.sh
+++ v2p.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
+# usage: v2p.sh INFILEPATH OUTFILEPATH -r CONVERTED_FPS
+## without -r argument
 
-mkdir $1_pics
-#echo ffmpeg -i %1 -r %2 -f image2 %1_pics\image-%%06d.png
-ffmpeg -i $1 -r $2 -f image2 $1_pics/image-%06d.png
+if [ -z $1 ]
+then
+	echo usage: v2p.sh INFILEPATH OUTFILEPATH -r CONVERTED_FPS
+	exit 0
+fi
 
-#pause
+infile=$1
+outfol=$2
+if [ -z "$outfol" ]
+then
+  outfol=$1_pics
+fi
+mkdir $outfol
+
+shift 2
+
+while getopts ":r" opt; do
+  case $opt in
+    r)
+    ffmpeg -i "$infile" -r $2 -f image2 "$outfol/image-%06d.png"
+    exit 0
+    ;;
+  esac
+done
+
+ffmpeg -i "$infile" -f image2 "$outfol/image-%06d.png"
