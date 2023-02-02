9d83c9abe46265a816ecc38efda5a0c841959fa5
Abel_ubun <21101710+AbelHo@users.noreply.github.com>
2023-02-02 14:18:20 +0800

    feat(pic2vid.sh): include arguments

--- pic2vid.sh
+++ pic2vid.sh
@@ -1,5 +1,24 @@
-ffmpeg -f image2 -framerate 50 -r 50 -i %%~nf/image-%04d.png -vcodec libx264 -b 5m %%~nf.mp4
+#!/usr/bin/env bash
 
-::	    ffmpeg -i %%~nf.mov -r 25 -f image2 %%~nf\image-%%04d.png -loglevel quiet
-    )
-::pause
+if [ -z $1 ]
+then
+	echo usage: pic2vid.sh INFILEPATH OUTFILEPATH -r CONVERTED_FPS
+	exit 0
+fi
+
+in_folder=$1
+out_video=$2
+fps=4
+
+shift 2
+while getopts ":r" opt; do
+  case $opt in
+    r)
+    fps=$2
+    #ffmpeg -i "$infile" -r $2 -f image2 "$outfol/image-%06d.png"
+    ;;
+  esac
+done
+
+ffmpeg -framerate $fps -i $in_folder/image-%06d.png -vcodec libx264 -b 5m -vf format=yuv420p $out_video
+
