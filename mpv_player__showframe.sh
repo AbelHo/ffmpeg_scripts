8c052fde2f8f959efa6c2d8a48b72797c3afad7f
AbelHo <21101710+AbelHo@users.noreply.github.com>
2024-10-04 18:14:33 +0800

    feat(mpv_player__showframe.sh): mpv player overlay frame number script

---
+++ mpv_player__showframe.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt install mpv
+mkdir ~/.config/mpv && mkdir ~/.config/mpv/scripts || echo "Directory already exist"
+echo  "mp.add_periodic_timer(1/30, function()
+    local time_pos = mp.get_property_number("time-pos", 0)
+    local hours = math.floor(time_pos / 3600)
+    local minutes = math.floor((time_pos % 3600) / 60)
+    local seconds = math.floor(time_pos % 60)
+    local milliseconds = math.floor((time_pos * 1000) % 1000)
+    local time_str = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    
+    local frame = mp.get_property_number("estimated-frame-number", 0)
+    mp.osd_message("Time: " .. time_str .. "\nFrame: " .. frame, 0.1)
+end)" > ~/.config/mpv/scripts/timeNframenumber.lua
