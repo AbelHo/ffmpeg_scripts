ff995802620113627c5c16f574f9541129b1aa61
AbelHo <21101710+AbelHo@users.noreply.github.com>
2025-09-04 16:29:00 +0800

    feat(mpv_player__showframe.sh): Add seconds display to OSD message in script

--- mpv_player__showframe.sh
+++ mpv_player__showframe.sh
@@ -9,7 +9,8 @@ echo  "mp.add_periodic_timer(1/30, function()
     local seconds = math.floor(time_pos % 60)
     local milliseconds = math.floor((time_pos * 1000) % 1000)
     local time_str = string.format("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+    local seconds_str = string.format("%.3f", time_pos)
     
     local frame = mp.get_property_number("estimated-frame-number", 0)
-    mp.osd_message("Time: " .. time_str .. "\nFrame: " .. frame, 0.1)
+    mp.osd_message("Time: " .. time_str .. "\nFrame: " .. frame .. "\ntime(s):" .. seconds_str, 0.1)
 end)" > ~/.config/mpv/scripts/timeNframenumber.lua
